f3ee200ae3491cbd2e4f78dce1ed9ace1e7a60a0
Michael Kainer <stuff@pushrax.com>
2026-06-27 21:03:12 +0200

    hass 8

--- configuration.nix
+++ configuration.nix
@@ -122,7 +122,7 @@
         NET_ADMIN = true;
       };
       devices = [
-        "/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_5266936139b6ed118c46d60ea8669f5d-if00-port0:/dev/ttyUSB0"
+        "/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_5266936139b6ed118c46d60ea8669f5d-if00-port0:/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_5266936139b6ed118c46d60ea8669f5d-if00-port0"
       ];
       image = "ghcr.io/home-assistant/home-assistant:stable";
       extraOptions = [ "--network=host" ];

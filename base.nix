9dc5dc3e27d0bcfde6cf9f1fb116afb50f084910
Michael Kainer <stuff@pushrax.com>
2026-06-28 20:46:42 +0200

    modules 6

--- base.nix
+++ base.nix
@@ -1,8 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [];
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./system.nix
+    ./base.nix
     ./haproxy.nix
   ];
 

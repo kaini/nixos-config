35a4dee70e79df5737597643904c642119c19230
Michael Kainer <stuff@pushrax.com>
2026-06-29 20:49:00 +0200

    obsidian

--- obsidian.nix
+++ obsidian.nix
@@ -1,9 +1,33 @@
 { config, lib, pkgs, ... }:
 
-let
-  obsidian-headless = pkgs.callPackage ./obsidian-headless {};
-in {
-  environment.systemPackages = [
-    obsidian-headless
+{
+  systemd.tmpfiles.rules = [
+    "d /var/lib/obsidian-vault 0770 10000 10000 -"
+    "d /var/lib/obsidian-config 0770 10000 10000 -"
   ];
+  
+  virtualisation.oci-containers = {
+    containers.obsidian-headless = {
+      image = "ghcr.io/belphemur/obsidian-headless-sync-docker:latest";
+      volumes = [
+        "/var/lib/obsidian-vault:/vault"
+        "/var/lib/obsidian-config:/home/obsidian/.config"
+      ];
+      environment = {
+        PUID = "10000";
+        PGID = "10000";
+        EXCLUDED_FOLDERS = "";
+      };
+      environmentFiles = [
+        # File contents:
+        # OBSIDIAN_AUTH_TOKEN=xxx
+        # VAULT_NAME=xxx
+        # VAULT_PASSWORD=xxx
+        "/home/michael/obsidian.secrets"
+      ];
+      cmd = [ "gateway" "run" ];
+      ports = [ "127.0.0.1:9119:9119" ];
+      extraOptions = [ "--shm-size=1g" ];
+    };
+  };
 }

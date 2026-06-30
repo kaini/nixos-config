c6fa7e44464cfbf76becba4b0883bf43af81176a
Michael Kainer <stuff@pushrax.com>
2026-06-30 21:04:46 +0200

    hindsight

--- base.nix
+++ base.nix
@@ -83,6 +83,15 @@
     };
   };
 
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
   virtualisation.oci-containers = {
     backend = "podman";
   };

e03359aea555de34f38f108747a4682bf7d33f93
Michael Kainer <stuff@pushrax.com>
2026-07-21 17:12:43 +0200

    Limit boot configurations to prevent out of space errors

--- base.nix
+++ base.nix
@@ -2,6 +2,7 @@
 
 {
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
 
   fileSystems."/".options = [ "noatime" ];
